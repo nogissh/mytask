ee0f4517569ba40d75e1b3ba2c3d7b45960bcba9
nogissh <nogissh@users.noreply.github.com>
2021-09-18 15:45:02 +0000

    deploy: 8bc9c12655f94615f23ded8ac421d2190426a2eb

---
+++ precache-manifest.d7771aa1b37f7d59efa5b44662c276a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2878cf6a4e09e7241ae5301b24a48a0",
+    "url": "/mytask/index.html"
+  },
+  {
+    "revision": "723e4951e3e50a5e5338fd3938c05cdb",
+    "url": "/mytask/manifest.json"
+  },
+  {
+    "revision": "ed5890b2b72829f5a2ca37d2c5b4b31d",
+    "url": "/mytask/manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mytask/robots.txt"
+  },
+  {
+    "revision": "f3aad52e699b5ddd04a3",
+    "url": "/mytask/static/css/app.e4ddd5bf.css"
+  },
+  {
+    "revision": "f3aad52e699b5ddd04a3",
+    "url": "/mytask/static/js/app.9b3b679b.js"
+  },
+  {
+    "revision": "b1c6b87c497670406352",
+    "url": "/mytask/static/js/chunk-vendors.af840a96.js"
+  }
+]);
