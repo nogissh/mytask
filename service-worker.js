10bf64be3f708172047a85df02dcb3d240d61f7c
nogissh <nogissh@users.noreply.github.com>
2021-10-14 13:42:45 +0000

    deploy: c9d4fff75d585c1c8fe240da20138d1512d4d3c7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mytask/precache-manifest.8a67002cf4468207c0a22fa860e1effa.js"
+  "/mytask/precache-manifest.5ac71651312fd658c779e4dc54c8299c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mytask"});
