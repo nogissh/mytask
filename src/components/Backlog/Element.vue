f4187c9b2e495a638d6f28440a641a5c924b685b
nogissh <thetoshiki0419@gmail.com>
2021-11-28 12:13:45 +0900

    add backlog menu

--- src/components/Backlog/Element.vue
+++ src/components/Backlog/Element.vue
@@ -6,9 +6,14 @@
       </span>
     </div>
     <div class="backlog__flex-operate" style="text-align: right">
-      <button class="action" style="margin-right: 8px" @click="toActive">Active</button>
-      <button class="action" style="margin-right: 8px" @click="goDeploy">Deploy</button>
-      <button @click.stop="del" class="action delete">Delete</button>
+      <button class="std antisquare" style="margin-right: 8px" @click="toActive">Active</button>
+      <span class="backlogmenu">
+        <button @click.stop="mutateBacklogmenuVisibility" class="std antisquare">...</button>
+        <ul v-if="visibility.backlogmenu">
+          <li @click.stop="goDeploy">Deploy</li>
+          <li @click.stop="del">Delete</li>
+        </ul>
+      </span>
     </div>
   </div>
 </template>
@@ -19,19 +24,31 @@ import { createNewTask } from '../../utils.js';
 export default {
   name: 'Element',
   props: ['task', 'index'],
+  data () {
+    return {
+      visibility: {
+        backlogmenu: false,
+      }
+    }
+  },
   methods: {
     toActive: function () {
       this.$store.dispatch('task/push', createNewTask(Date.now(), this.task.name));
       this.$store.dispatch('backlog/delete', this.task.id);
     },
     goDeploy: function () {
+      this.mutateBacklogmenuVisibility();
       this.$router.push({ name: 'BacklogDeployment', params: { id: this.task.id }})
     },
     del: function () {
       if (! confirm('Deleted task are never restore. [y/N]')) {
         return;
       }
+      this.mutateBacklogmenuVisibility();
       this.$store.dispatch('backlog/delete', this.task.id);
+    },
+    mutateBacklogmenuVisibility: function () {
+      this.visibility.backlogmenu = !this.visibility.backlogmenu;
     },
   }
 }
@@ -44,9 +61,33 @@ export default {
   justify-content: space-between;
 }
 .backlog__flex-name {
-  flex-basis: 70%;
+  flex-basis: 80%;
 }
 .backlog__flex-operate {
-  flex-basis: 30%;
+  flex-basis: 20%;
+}
+
+.backlogmenu button:hover {
+  background-color: gray;
+}
+.backlogmenu ul {
+  list-style: none;
+  position: absolute;
+  text-align: center;
+  font-size: 13px;
+  width: 100px;
+  margin: 0;
+  padding: 8px 0;
+  background-color: white;
+  box-shadow: rgb(9 30 66 / 25%) 0px 4px 8px -2px, rgb(9 30 66 / 31%) 0px 0px 1px;
+  border-radius: 3px;
+  transform: translate3d(80px, 16px, 0px);
+}
+.backlogmenu ul li {
+  cursor: pointer;
+  padding: 4px 0;
+}
+.backlogmenu ul li:hover {
+  background-color: #eee;
 }
 </style>
